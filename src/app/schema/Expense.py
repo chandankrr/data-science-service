2e9ff7b1ae79b4f905221a75f38bbe8f38d6cd71
Chandan Kumar <chandankrr.91@gmail.com>
2024-05-01 00:22:27 +0530

    Create data science model to extract details from message

---
+++ src/app/schema/Expense.py
@@ -0,0 +1,7 @@
+from langchain_core.pydantic_v1 import BaseModel, Field
+from typing import Optional
+
+class Expense(BaseModel):
+    amount: Optional[str] = Field(title="expense", description="Expense made in the transaction")
+    merchant: Optional[str] = Field(title="merchant", description="Merchant name whom the transaction has been made")
+    currency: Optional[str] = Field(title="currency", description="Currency of the transaction")
---
+++ src/app/service/llmService.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from typing import Optional
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.pydantic_v1 import BaseModel ,Field
+from langchain_openai import ChatOpenAI
+from langchain_mistralai import ChatMistralAI
+from langchain_core.utils.function_calling import convert_to_openai_tool
+
+from schema.Expense import Expense
+
+class LLMService:
+    def __init__(self):
+        load_dotenv
+        self.prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You are an expert extraction algorithm. "
+                    "Only extract relevant information from the text. "
+                    "If you do not know the value of an attribute asked to extract, "
+                    "return null for the attribure's value."
+                ),
+                ("human", "{text}")
+            ]
+        )
+        self.apiKey = os.getenv('OPENAI_API_KEY')
+        self.llm = ChatMistralAI(api_key=self.apiKey, model="mistral-large-latest")
+        self.runnable = self.prompt | self.llm.with_structured_output(schema=Expense)
+
+    def runLLM(self, message):
+        return self.runnable.invoke({"text": message})
