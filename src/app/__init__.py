9505c52f070c5129e53b9ce399b3c9bf4f8cdc07
Chandan Kumar <chandankrr.91@gmail.com>
2024-05-24 20:05:49 +0530

    Integrate kafka and create producer to send expense data to kafka

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask, request, jsonify
+from kafka import KafkaProducer
+import json
 from service.messageService import MessageService
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/ds/message', methods=['POST'])
 def handle_message():
     message = request.json.get('message')
     result = messageService.process_message(message)
+    serialized_result = result.json()
+
+    # send the serialized result to kafka topic
+    producer.send('expense_service', serialized_result)
+    
     return jsonify(result)
 
 @app.route("/", methods=['GET'])
