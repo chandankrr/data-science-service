bc60131d03118e352c1d077c5569ca7a285fff15
Chandan Kumar <chandankrr.91@gmail.com>
2024-07-22 18:39:18 +0530

    Change expense schema to get exact value of expense amount

--- src/app/schema/Expense.py
+++ src/app/schema/Expense.py
@@ -4,7 +4,7 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 
 class Expense(BaseModel):
         """Information about a transaction made on any Card"""
-        amount: Optional[str] = Field(title="expense", description="Expense made on the transaction only amount")
+        amount: Optional[str] = Field(title="expense", description="Expense made on the transaction, extract amount, if ammount is in whole number or floating point number extract only amount value and nothing else")
         merchant: Optional[str] = Field(title="merchant", description="Merchant name whom the transaction has been made")
         currency: Optional[str] = Field(title="currency", description="currency of the transaction")
     
